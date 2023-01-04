a642481927da9f51926573514fd1f448db73f1c0
Kevin Green <stuckbykev@gmail.com>
2023-01-04 14:38:13 -0800

    Added public access init.

--- Sources/KGViews/KGRoundedCorner.swift
+++ Sources/KGViews/KGRoundedCorner.swift
@@ -11,6 +11,12 @@ import SwiftUI
 public struct KGRoundedCorner: Shape {
     public var corners: UIRectCorner
     public var radius: CGFloat
+    
+    public init(corners: UIRectCorner, radius: CGFloat) {
+        self.corners = corners
+        self.radius = radius
+    }
+    
     public func path(in rect: CGRect) -> Path {
         let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
         return Path(path.cgPath)
