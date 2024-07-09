eada5c2ea7d9f6a6bc161049bf1f8c6bcab6d6f7
Kevin Green <stuckbykev@gmail.com>
2024-07-09 15:25:26 -0700

    [Feature] Added new Image initiizer.

--- Sources/KGViews/Images.swift
+++ Sources/KGViews/Images.swift
@@ -1,18 +1,23 @@
 //
-//  SwiftUIView.swift
-//  
+//  Image.swift
 //
-//  Created by Kevin Green on 7/9/24.
+//  Created by Kevin Green on 10/19/23.
 //
 
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public extension Image {
+    
+    /// Initializes an new Image view with the UIImage passed or a system icon.
+    /// - Parameters:
+    ///   - uiimage: An optional UIImage object.
+    ///   - systemName: The name of an SF symbol. Default if "person.fill":
+    init(uiimage: UIImage?, systemName: String = "person.circle.fill") {
+        if let uiimage = uiimage {
+            self.init(uiImage: uiimage)
+        } else {
+            self.init(systemName: systemName)
+        }
     }
-}
-
-#Preview {
-    SwiftUIView()
+    
 }
