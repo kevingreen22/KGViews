a45a1019efc2c7b3a43bab99b52b0d37d21a4b05
Kevin Green <stuckbykev@gmail.com>
2024-07-09 14:07:19 -0700

    Added public access to SmoothDrag.

--- Sources/KGViews/Gestures.swift
+++ Sources/KGViews/Gestures.swift
@@ -11,11 +11,15 @@ public struct SmoothDrag: Gesture {
     @Binding var location: CGSize
     @GestureState private var startLocation: CGSize? = nil
     
+    public init(location: Binding<CGSize>) {
+        _location = location
+    }
+    
     public var body: some Gesture {
         simpleDrag
     }
     
-    var simpleDrag: some Gesture {
+    public var simpleDrag: some Gesture {
         DragGesture()
             .onChanged { value in
                 var newLocation = startLocation ?? location
